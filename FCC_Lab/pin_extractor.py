e6077f72e1e8d30e15e9e981905207d8479ca934
Helen B. <elabrsh123@gmail.com>
2026-07-18 16:27:54 +0300

    Here is the whole source code

--- FCC_Lab/pin_extractor.py
+++ FCC_Lab/pin_extractor.py
@@ -10,3 +10,11 @@ def pin_extractor(poems):
             else:
                 secret_code += '0'
         secret_codes.append(secret_code)
+poem = """Stars and the moon
+shine in the sky
+white and
+until the end of the night"""
+
+poem2 = 'The grass is green\nhere and there\nhoping for rain\nbefore it turns yellow'
+poem3 = 'There\nonce\nwas\na\ndragon'
+print(pin_extractor([poem,poem2.poem3]))
