b5cf05357bcfdc9f6acd7e559ba569f4f0fdce10
Helen B. <elabrsh123@gmail.com>
2026-07-22 11:04:04 +0300

    Here is the whole code

--- Python_Daily_Code_Challenge/piggy_Bank.py
+++ Python_Daily_Code_Challenge/piggy_Bank.py
@@ -12,3 +12,15 @@ def piggy_bank(coins):
         value=(coins[key])
         amount= (coin_values.get(key_name))
         result += (amount*value)
+
+    return f"${result:.2f}"
+
+print(piggy_bank({"pennies": 3, "nickels": 5, "dimes": 2, "quarters": 6}))
+
+print(piggy_bank({"pennies": 1, "nickels": 1, "dimes": 1, "quarters": 1}))
+
+print(piggy_bank({"nickels": 8, "dimes": 6, "quarters": 5}))
+
+print(piggy_bank({}))
+
+print(piggy_bank({"pennies": 146, "nickels": 11, "dimes": 0, "quarters": 19}))
