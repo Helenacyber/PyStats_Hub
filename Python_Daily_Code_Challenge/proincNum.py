184747241d74310b3e17b47a68660c0265d299eb
Helen B. <elabrsh123@gmail.com>
2026-07-28 10:31:49 +0300

    added a function that checks whether a given number is the product of two consecutive numbers

--- Python_Daily_Code_Challenge/proincNum.py
+++ Python_Daily_Code_Challenge/proincNum.py
@@ -1,1 +1,10 @@
 #This program defines a function that takes a number and check it wherether the number is a pronic or not \n A pronic A pronic number is the product of two consecutive integers. For example, 6 is pronic because 2 * 3 = 6.
+def is_pronic (num):
+    num_range= range(0,num+1)
+    for a ,b in enumerate(num_range,1):
+        if abs(a-b) == 1:
+           if a*b == num:
+              return True
+    return False
+
+
